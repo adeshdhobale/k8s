c65fff13ffa31673665c495cf774acc846b80b40
= <adeshdhobale@gmail.com>
2025-02-21 15:27:36 +0530

    chaged project

--- src/main/java/com/k8s/demo/controllers/DemoController.java
+++ src/main/java/com/k8s/demo/controllers/DemoController.java
@@ -3,11 +3,14 @@ package com.k8s.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/demo")
 public class DemoController  {
 
 	@GetMapping("/index")
+	@ResponseBody
 	public String getIndexPage()
 	{
 		return "index";
